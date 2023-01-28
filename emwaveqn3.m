68ebcad24ad58d0e0ced3e27c8f47d46b0a625fb
Nithin2k02 <123719344+Nithin2k02@users.noreply.github.com>
2023-01-28 19:47:13 +0530

    Add files via upload
    Visualization of CP
    3:LHCP
    3b:RHCP

---
+++ emwaveqn3.m
@@ -0,0 +1,56 @@
+close all; clear all; clc;
+j=0;b=2;w=5;%i is time
+z=linspace(0,8,1000);
+m=zeros(1,1000);
+
+for j=1:0.01:5
+    i=j-1;
+    E1=cos(w*i-b*z+pi/2);
+    E2=cos(w*i-b*z);
+    E=E1+E2;
+    axis([0 1 0 1 0 1])
+    
+    
+    
+    p1=plot3(z,E1,m,'r');
+    hold on
+    p2=plot3(z,m,E2,'k');
+    hold on
+    p3=plot3(z,E1,E2,'b');
+    quiver3(0,0,0,0,0,cos(w*i),'k','LineWidth',3);
+    quiver3(0,0,0,0,cos(w*i+pi/2),0,'r','LineWidth',3);
+    quiver3(0,0,0,0,cos(w*i+pi/2),cos(w*i),'b','LineWidth',3);
+    
+    p1.LineWidth=1;
+    p2.LineWidth=1;
+    p3.LineWidth=3;
+    
+    hold on
+    quiver3(0,0,-max(zlim),0,0,2*max(zlim),'b','LineWidth',1);
+    quiver3(0,-max(ylim),0,0,2*max(ylim),0,'b','LineWidth',1);
+    quiver3(0,0,0,max(xlim),0,0,'b','LineWidth',1);
+    text(0,0,max(zlim),'Y','Color','b');
+    text(0,max(ylim),0,'X','Color','b');
+    text(max(xlim),0,0,'Z','Color','b');
+    axis equal;
+    view(30,30);
+    set(gca, 'LineWidth',2, 'XGrid','on', 'GridLineStyle','--');
+
+    zlabel("\color{black}E1(z)");
+    ylabel("\color{red}E2(z)");
+    xlabel("z");
+    
+    title('LHCP','Color','k','FontSize',20)
+    
+    
+    
+    if j==1
+        text(0,0,0,'Initially starting with a phase difference of pi/2 with Ey leading','Color','red','FontSize',14)
+        pause(5);
+    end
+   hold off
+   
+   
+    pause(0.05);
+end
+
